23cee2828a577d0cb3d74cf6c37db1b65ed0ad06
Maxfirex <35975537+Maxfirex@users.noreply.github.com>
2018-11-20 20:15:46 +0100

    Map generator
    Before color maps

---
+++ Assets/CustomEditor/MapGeneratorEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        //if changes are made in editor and autoUpdate is true, update map
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+        //On button generate, update map with given values
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
